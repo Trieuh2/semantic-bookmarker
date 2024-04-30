edebfaee6636ac66f26d3a899a70bb5ecfe237e4
henrytrieu88@gmail.com <henrytrieu88@gmail.com>
2024-04-30 14:28:37 -0700

    feat(frontend): Update extension TextArea focus handling

--- browser-extension/src/pages/popup/components/bookmarkForm/TextArea.tsx
+++ browser-extension/src/pages/popup/components/bookmarkForm/TextArea.tsx
@@ -1,82 +1,142 @@
 import clsx from "clsx";
 import React, { useEffect, useRef, useState } from "react";
+import { useBookmarks } from "../../../../context/BookmarkContext";
 
 interface TextAreaProps {
-  value?: string;
   useBackground?: boolean;
-  useUnderline?: boolean;
-  onTextChange: (value: string) => void;
-  onBlur: () => void;
+  isTitle?: boolean;
+  field: "title" | "page_url" | "note";
 }
 
 const TextArea: React.FC<TextAreaProps> = ({
-  value = "",
   useBackground,
-  useUnderline,
-  onTextChange,
-  onBlur,
+  isTitle,
+  field,
 }) => {
+  const { state, dispatch } = useBookmarks();
   const [isFocused, setIsFocused] = useState(false);
-  const [text, setText] = useState<string>(value);
+  const textRef = useRef<string>("");
+  const [text, setText] = useState<string>("");
   const textareaRef = useRef<HTMLTextAreaElement>(null);
 
   useEffect(() => {
-    setText(value);
-  }, [value]);
+    textRef.current = state[field];
+    setText(state[field]);
+  }, [state.title, state.page_url, state.note]);
 
   useEffect(() => {
     if (textareaRef.current) {
-      if (isFocused) {
+      if (isTitle || isFocused) {
         textareaRef.current.style.height = "auto";
         textareaRef.current.style.height = `${textareaRef.current.scrollHeight}px`;
       } else {
         textareaRef.current.style.height = "2rem";
       }
     }
-  }, [isFocused, text]);
+  }, [isTitle, isFocused, text]);
+
+  const handleTextAreaOnChange = (
+    field: "title" | "page_url" | "note",
+    value: string
+  ) => {
+    textRef.current = value;
+    setText(value);
+    dispatch({
+      type: "SET_STATE",
+      variable: field as "title" | "note" | "page_url",
+      payload: value,
+    });
+  };
+
+  // Reset the textArea to initial values if the field becomes empty
+  const handleTextAreaOnBlur = (field: "title" | "page_url" | "note") => {
+    if (
+      (field === "title" || field === "page_url") &&
+      !state[field] &&
+      state.initialValues
+    ) {
+      dispatch({
+        type: "SET_STATE",
+        variable: field,
+        payload: state.initialValues[field],
+      });
+    }
+  };
+
+  const divContainerClasses = clsx(
+    `
+    w-full
+    max-w-[305px]
+    max-h-[168px]
+    h-15
+    flex
+    flex-col
+    mx-2
+    items-center
+    justify-center
+    relative
+    bg-zinc-800
+    `
+  );
+
+  const scrollbarClasses = clsx(
+    `
+    scrollbar-thin
+    scrollbar-thumb-rounded-md
+    scrollbar-thumb-neutral-600
+    scrollbar-track-rounded-md
+    scrollbar-track-neutral-500
+    overflow-y-scroll
+  `
+  );
 
   const textAreaClasses = clsx(
     `
     w-full
     box-border
-    py-2
+    py-1.5
     px-2
-    mx-2
     rounded-md
     resize-none
-    border-transparent
     bg-transparent
-    text-start
+    text-white
+    text-sm
     break-all
-    outline-none
+    focus:outline-none
     transition`,
+    scrollbarClasses,
     useBackground && "bg-zinc-900",
-    !useUnderline && "focus:ring-2 focus:ring-orange-300 focus:bg-transparent"
+    isTitle && "outline-none",
+    !isTitle && "focus:ring-2 focus:ring-orange-300 focus:bg-transparent",
+    !isTitle && !isFocused && "leading-relaxed",
+    !isTitle && isFocused && "leading-normal"
   );
 
   return (
-    <div className="w-full flex text-white items-center justify-center relative bg-zinc-800">
+    <div className={divContainerClasses}>
       <textarea
         ref={textareaRef}
         className={textAreaClasses}
         value={text}
-        onChange={(e) => onTextChange(e.target.value)}
+        onChange={(e) => handleTextAreaOnChange(field, e.target.value)}
         onBlur={() => {
+          if (textareaRef.current) {
+            textareaRef.current.scrollTop = 0;
+          }
           setIsFocused(false);
-          onBlur();
+          handleTextAreaOnBlur(field);
         }}
-        onFocus={() => setIsFocused(true)}
         rows={1}
+        onFocus={() => setIsFocused(true)}
         style={{
           overflowY: isFocused ? "auto" : "hidden",
         }}
       ></textarea>
       {/* Underline element */}
-      {useUnderline && isFocused && (
+      {isTitle && (
         <div
           className={clsx(
-            "absolute bottom-0 left-0 right-0 h-0.5 mx-2",
-            isFocused && "bg-orange-300"
+            "absolute bottom-0 left-0 right-0 h-0.5 mx-2 bg-orange-300"
           )}
         ></div>
       )}
