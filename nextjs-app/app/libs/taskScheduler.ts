04aa3806869eef7fa658b5140fe9e2448c041a4a
henrytrieu88@gmail.com <henrytrieu88@gmail.com>
2024-05-16 15:16:51 -0700

    feat(backend): optimize task scheduler
    - Implement dynamic lock expiration. This extends extends the expiration time of the lock every 0.5s to ensure that the current batch of tasks finishes.\n- Change use of interval to timeout for main processing function of taskScheduler. This change is needed since setTimeout only schedules the next task to run if the previous task finished. The previous use of setInterval could potentially cause memory exhaustion and a memory leak due to long-running tasks that take over 1s and the looping logic queueing up additional batches.

--- nextjs-app/app/libs/taskScheduler.ts
+++ nextjs-app/app/libs/taskScheduler.ts
@@ -5,7 +5,7 @@ import redis from "./redis";
 async function setupIntervalJobs() {
   console.log("Setting up interval jobs...");
 
-  setInterval(async () => {
+  const runProcess = async () => {
     const lock = await redis.set(
       "lock:processBatchUpdates",
       "true",
@@ -13,16 +13,44 @@ async function setupIntervalJobs() {
       10,
       "NX"
     );
+
     if (lock) {
+      let lockExtender: NodeJS.Timeout | null = null;
+
       try {
+        // Function to extend the lock duration if batch still processing
+        const extendLock = async () => {
+          const result = await redis.expire("lock:processBatchUpdates", 10);
+          if (result === 0) {
+            throw new Error("Failed to extend lock.");
+          }
+        };
+
+        // Periodically extend lock every 0.5s
+        lockExtender = setInterval(async () => {
+          try {
+            await extendLock();
+          } catch (error) {
+            console.error("Failed to extend lock:", error);
+          }
+        }, 500);
+
         await processBatchUpdates();
+      } catch (error) {
+        console.error("Error during batch update processing:", error);
       } finally {
+        if (lockExtender) {
+          clearInterval(lockExtender);
+        }
         await redis.del("lock:processBatchUpdates");
       }
     } else {
       console.log("Process is already running.");
     }
-  }, 1000); // Runs every second, but ensures no overlap
+    // Schedule the next run
+    setTimeout(runProcess, 1000); // Runs every second
+  };
+  runProcess();
 }
 
 setupIntervalJobs();
